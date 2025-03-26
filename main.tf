319feb40246101d82435ed8bdb458013fcec74a8
Piterxus <piterxus@gmail.com>
2025-03-26 19:13:48 +0100

    change terraform

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "aws_instance" "app_server" {
             
             # Clonar el repositorio de GitHub
             cd /home/ec2-user
-            git clone https://github.com/jorloque/mi-app-tf.git
+            git clone https://github.com/Piterxus/express-docker-terraform-aws.git
             cd mi-app-tf
             
             # Construir y ejecutar el contenedor Docker
